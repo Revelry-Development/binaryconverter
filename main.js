7b4100b2589cefdb5efaa060994b744d580a03f8
Spencer Kordecki <spencerkordecki@users.noreply.github.com>
2016-10-12 19:45:10 -0400

    Development -> Master (#1)
    * vanilla
    
    * untested finish
    
    * unpacked mode working

--- main.js
+++ main.js
@@ -1,85 +1,142 @@
-/*
- * Main driver for the application; contains logic for conversion between
- * decimal, binary, hexadecimal, and octal.
- *
- * @author: David Dymko, Spencer Kordecki
- */
+(function() {
+    document.addEventListener('DOMContentLoaded', function() {
+        var modifier;
+        var numb, bina, octa, hexa;
+        var dec = document.getElementById('dec');
+        var bin = document.getElementById('bin');
+        var oct = document.getElementById('oct');
+        var hex = document.getElementById('hex');
 
-$(document).ready(function() {
+        // Validates the inputs
+        dec.addEventListener('keydown', function(e) {
+            if (!validateDec(e.keyCode) && !validateBindings(e)) e.preventDefault();
+        });
 
-  var numb, bina, octa, hexa;
+        // Converts the values, clears input boxes otherwise
+        dec.addEventListener('keyup', function(e) {
+            if (isEmpty(this.value))
+                clear();
+            else if (!allowTabbing(e)) {
+                convert(this.value);
+                bin.value = bina;
+                oct.value = octa;
+                hex.value = hexa;
+            }
+        });
 
-  $('#dec').keyup(function() {
-    var decimal = $('#dec').val();
+        // Validates the inputs
+        bin.addEventListener('keydown', function(e) {
+            if (!validateBin(e.keyCode) && !validateBindings(e)) e.preventDefault();
+        });
 
-    if (isEmpty(decimal)) {
-      clear();
-    }
-    else if (!isNaN(decimal)) {
-      convert(decimal);
-      $('#bin').val(bina);
-      $('#oct').val(octa);
-      $('#hex').val(hexa);
-    }
-  });
+        // Converts the values, clears input boxes otherwise
+        bin.addEventListener('keyup', function(e) {
+            if (isEmpty(this.value))
+                clear();
+            else if (!allowTabbing(e)) {
+                convert(this.value);
+                dec.value = numb;
+                oct.value = octa;
+                hex.value = hexa;
+            }
+        });
 
-  $('#bin').keyup(function() {
-    var binary = $('#bin').val();
+        // Validates the inputs
+        oct.addEventListener('keydown', function(e) {
+            if (!validateOct(e.keyCode) && !validateBindings(e)) e.preventDefault();
+        });
 
-    if (isEmpty(binary)) {
-      clear();
-    }
-    else if (!isNaN(binary)) {
-      convert(parseInt(binary, 2));
-      $('#dec').val(numb);
-      $('#oct').val(octa);
-      $('#hex').val(hexa);
-    }
-  });
+        // Converts the values, clears input boxes otherwise
+        oct.addEventListener('keyup', function(e) {
+            if (isEmpty(this.value))
+                clear();
+            else if (!allowTabbing(e)) {
+                convert(this.value);
+                bin.value = bina;
+                dec.value = numb;
+                hex.value = hexa;
+            }
+        });
 
-  $('#oct').keyup(function() {
-    var octal = $('#oct').val();
+        // Validates the inputs
+        hex.addEventListener('keydown', function(e) {
+            if (!validateHex(e.keyCode) && !validateBindings(e)) e.preventDefault();
+        });
 
-    if (isEmpty(octal)) {
-      clear();
-    }
-    else if (!isNaN(octal)) {
-      convert(parseInt(octal, 8));
-      $('#dec').val(numb);
-      $('#bin').val(bina);
-      $('#hex').val(hexa);
-    }
-  });
+        // Converts the values, clears input boxes otherwise
+        hex.addEventListener('keyup', function(e) {
+            if (isEmpty(this.value))
+                clear();
+            else if (!allowTabbing(e)) {
+                convert(parseInt(this.value, 16));
+                bin.value = bina;
+                dec.value = numb;
+                oct.value = octa;
+            }
+        });
 
-  $('#hex').keyup(function() {
-    var hexadecimal = $('#hex').val();
+        // Power the conversions to the respective format
+        function convert(val) {
+            numb = new Number(val);
+            bina = numb.toString(2);
+            octa = numb.toString(8);
+            hexa = numb.toString(16).toUpperCase();
+        }
 
-    if (isEmpty(hexadecimal)) {
-      clear();
-    }
-    else if ((97 < String.charCodeAt(hexadecimal.toLowerCase()) < 102) || !isNaN(hexadecimal)) {
-      convert(parseInt(hexadecimal, 16));
-      $('#dec').val(numb);
-      $('#bin').val(bina);
-      $('#oct').val(octa);
-    }
-  });
+        // Checks if the value is empty
+        function isEmpty(value) {
+            return value === '';
+        }
 
-  function convert(val) {
-    numb = new Number(val);
-    bina = numb.toString(2);
-    octa = numb.toString(8);
-    hexa = numb.toString(16);
-  }
+        // Clears out the text boxes when the user deletes an entry
+        function clear() {
+            dec.value = '';
+            bin.value = '';
+            hex.value = '';
+            oct.value = '';
+        }
 
-  function isEmpty(value) {
-    return value === '';
-  }
+        // Validates the inputs for decimal boxes
+        function validateDec(keyCode) {
+            return ((keyCode >= 48 && keyCode <= 57) || (keyCode >= 96 && keyCode <= 105)) ? true : false;
+        }
 
-  function clear() {
-    $('#dec').val('');
-    $('#bin').val('');
-    $('#hex').val('');
-    $('#oct').val('');
-  }
-});
+        // Validates the inputs for binary boxes
+        function validateBin(keyCode) {
+            return (keyCode === 48 || keyCode === 49 || keyCode === 96 || keyCode === 97) ? true : false;
+        }
+
+        // Validates the inputs for octal boxes
+        function validateOct(keyCode) {
+            return ((keyCode >= 48 && keyCode <= 55) || (keyCode >= 96 && keyCode <= 103)) ? true : false;
+        }
+
+        // Validates the inputs for hexadecimal boxes
+        function validateHex(keyCode) {
+            return ((keyCode >= 48 && keyCode <= 57) || (keyCode >= 96 && keyCode <= 105) || (keyCode >= 65 && keyCode <= 70)) ? true : false;
+        }
+
+        // Validates when various bindings are used for ease of use, outlined below
+        function validateBindings(ev) {
+            if (ev.keyCode === 8) // Backspace key
+                return true;
+            if (ev.keyCode === 9) // Tab key
+                return true;
+            if (ev.keyCode === 46) // Delete key
+                return true;
+            if (ev.keyCode === 65 && ev.metaKey === true) // Cmd + A
+                return true;
+            if (ev.keyCode === 65 && ev.ctrlKey === true) // Ctrl + A
+                return true;
+            if (ev.keyCode >= 37 && ev.keyCode <= 40) // Arrow keys
+                return true;
+        }
+
+        // Allows for the use to tab and shift tab through the results
+        function allowTabbing(ev) {
+            console.log(ev.keyCode + ', Shift:' + ev.shiftKey);
+            if (ev.keyCode === 9 || ev.shiftKey && ev.keyCode === 9 || ev.keyCode === 16)
+                return true;
+        }
+    });
+})();
